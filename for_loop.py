877165fe9eb5e6a9d6e05f724f954ed52b5e7cc3
kNosek1618 <k.nosek@ievent-management.pl>
2019-06-11 15:58:21 +0200

    fun with for loop

---
+++ for_loop.py
@@ -0,0 +1,30 @@
+
+for letter in "Konrad Noise":
+     print(letter)
+
+print("")
+
+friends = ["Jim", "Karen", "Kevin"]
+# len() is function for counting the length of letters, elements or numbers
+
+for index in range(len(friends)):
+    print(friends[index])
+
+print("")
+
+friends = ["Jim", "Karen", "Kevin"]
+
+for friend in friends:
+    print(len(friend))
+
+print("")
+
+range = range(0,10)
+for i in range:
+    print(i)
+
+print("")
+
+my_list = ["ene", "due", "rike", "fake"]
+for i in my_list:
+    print(i)
